2227df06fbcf761bf5df297b2ed69a2876c6e532
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-21 10:49:04 +0000

    add const called messages

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const button = document.getElementById("button");
 const toasts = document.getElementById("toasts");
+
+const messages = [
+  "Messange One",
+  "Messange Two",
+  "Messange Three",
+  "Messange Four",
+];
