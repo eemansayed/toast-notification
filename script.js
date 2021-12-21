dcca37836fee1f7d04a70322c611987d7e42e136
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-21 10:52:41 +0000

    add function called createNotification

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ const messages = [
 ];
 
 button.addEventListener("click", () => createNotification());
+
+function createNotification() {
+  const notif = document.createElement("div");
+  notif.classList.add("toast");
+
+  notif.innerText = getRandomMessage();
+}
